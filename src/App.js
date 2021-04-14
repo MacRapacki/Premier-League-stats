d49eeb5ace13f5727941b8a2dd5ded70beb32ddc
Maciej Rapacki <mac.rapacki@gmail.com>
2021-04-14 21:43:51 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.scss";
 
 import Table from "./components/Table";
 import Nav from "./components/layout/Nav";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <>
       <Nav />
       <Table />
+      <Footer />
     </>
   );
 }
--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const Nav = () => {
   return (
     <nav className="navigation">
-      <div className="nav_logo">logo</div>
-      <ul className="nav_list">
-        <li className="nav_item">Table</li>
-        <li className="nav_item">Players Stats</li>
-      </ul>
+      <div className="nav_wrapper">
+        <div className="nav_logo">logo</div>
+        <ul className="nav_list">
+          <li className="nav_item">Table</li>
+          <li className="nav_item">Players Stats</li>
+        </ul>
+      </div>
     </nav>
   );
 };
