89a0b0fdb2911eca280b5ff286f439255f636492
Maciej Rapacki <mac.rapacki@gmail.com>
2021-05-10 18:24:49 +0100

    add logo to nav

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+
+import Logo from "../../images/premier-league-1.svg";
 
 const Nav = () => {
   return (
     <nav className="navigation">
       <div className="nav_wrapper">
-        <div className="nav_logo">EPL</div>
+        <Link to="/" exact className="nav_logo">
+          <img src={Logo} alt="premier league logo" />
+        </Link>
         <ul className="nav_list">
           <NavLink className="nav_item" activeClassName="selected" to="/" exact>
             Home
