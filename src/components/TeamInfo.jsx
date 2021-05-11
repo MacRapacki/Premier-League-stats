1d0e54115c43e72b6bf1c2eed1e431dc4e845e91
Maciej Rapacki <mac.rapacki@gmail.com>
2021-05-11 22:12:23 +0200

    add teamInfo component, add style, add image

--- src/components/TeamInfo.jsx
+++ src/components/TeamInfo.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const TeamInfo = () => {
-  return <section className="team_info"></section>;
+const TeamInfo = ({ teamID, setTeamInfoActive }) => {
+  const [teamData, setTeamData] = useState([]);
+
+  const getTeamData = async () => {
+    const url = `http://api.football-data.org/v2/teams/${teamID}`;
+    const options = {
+      type: "GET",
+      headers: { "X-Auth-Token": "3fccf6a31f3e4b9fa28393d33f768c10" },
+      dataType: "json",
+    };
+
+    const resp = await fetch(url, options);
+    if (!resp.ok) {
+      const message = `status is: ${resp.status}`;
+      throw new Error(message);
+    }
+    const data = await resp.json();
+    return data;
+  };
+
+  useEffect(() => {
+    getTeamData()
+      .then((data) => setTeamData(data))
+      .catch((error) => console.log("error!"));
+  }, []);
+
+  console.log(teamData);
+  return (
+    <div className="team_overlay">
+      <div className="team_info">
+        <button
+          type="button"
+          className="team_info_closeBtn"
+          onClick={() => setTeamInfoActive(false)}
+        >
+          X
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TeamInfo;
--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -6,6 +6,8 @@ import TeamInfo from "../components/TeamInfo";
 
 const Teams = () => {
   const [teams, setTeams] = useState([]);
+  const [teamInfoActive, setTeamInfoActive] = useState(false);
+  const [teamID, setTeamID] = useState(1);
 
   const getTeams = async () => {
     const url = "http://api.football-data.org/v2/competitions/2021/teams";
@@ -23,6 +25,11 @@ const Teams = () => {
     return data;
   };
 
+  const openTeamInfo = (id) => {
+    setTeamInfoActive(true);
+    setTeamID(id);
+  };
+
   useEffect(() => {
     getTeams()
       .then((data) => setTeams(data))
@@ -30,23 +37,33 @@ const Teams = () => {
   }, []);
 
   return (
-    <section className="teams">
-      {teams?.teams?.map((team) => {
-        const { crestUrl, id, name } = team;
-        return (
-          <Link
-          //   component={TeamInfo}
-          >
+    <>
+      <section className="teams">
+        {teams?.teams?.map((team) => {
+          const { crestUrl, id, name } = team;
+          return (
             <div className="teams_team" key={uuid()}>
               <div className="team_img">
                 <img src={crestUrl} alt="team logo" />
               </div>
               <p className="team_name">{name}</p>
+              <div className="team_info_overlay">
+                <button
+                  type="button"
+                  className="team_button"
+                  onClick={() => openTeamInfo(id)}
+                >
+                  Find out more
+                </button>
+              </div>
             </div>
-          </Link>
-        );
-      })}
-    </section>
+          );
+        })}
+        {teamInfoActive && (
+          <TeamInfo setTeamInfoActive={setTeamInfoActive} teamID={teamID} />
+        )}
+      </section>
+    </>
   );
 };
 
