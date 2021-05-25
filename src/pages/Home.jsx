da5b10e9a7eedb1e6ab4def6d077753737e10789
Maciej Rapacki <mac.rapacki@gmail.com>
2021-05-25 20:58:21 +0200

    add text to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,16 @@ const Home = () => {
   return (
     <>
       <header>
-        <img
+        {/* <img
           className="header_img"
           src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Premier_League_Logo.svg/1920px-Premier_League_Logo.svg.png"
           alt="Premier League Logo"
-        />{" "}
-        <h1 className="header_title">Premier League</h1>
+        />{" "} */}
+        <h1 className="header_title">Premier League </h1>
+        <p className="header_info">
+          This website is for Premier League 20/21 fans. Here You can check
+          Teams and Players basis statistics. Enjoy!{" "}
+        </p>
       </header>
       <section className="home"></section>
     </>
